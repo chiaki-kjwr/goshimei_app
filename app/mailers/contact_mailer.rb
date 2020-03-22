f79a0c99dfccc7aaa71a4ecfb49b4efca9324d0c
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-03-22 19:50:38 +0900

    Add DatabassToDatabeseyml

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
     def contact_mail(contact)
         @contact = contact
         mail to:"chiakikajiwara@gmail.com",subject: "confirmation"
-      end
     end
 end
+
