a6f8b9e24f380804f39866880901da0024352d81
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-03-23 22:59:37 +0900

    Fix top_page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get  'pages/help'
   get  'pages/terms'
 
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show', as: 'user'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :contacts
   
