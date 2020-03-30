00c9917e7115c9899d2c7f81b097cf1000debf7b
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-03-30 19:52:49 +0900

    Fix user_edit_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: { registrations: 'registrations' }
   
   root 'pages#home'
   get  'pages/about'
   get  'pages/help'
   get  'pages/terms'
 
-  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id', to: 'users#show'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :contacts
   resources :posts, only: %i(new create index show) 
-  resources :companies,only: %i(new create indexshow edit update) 
+  
 
 end
 
