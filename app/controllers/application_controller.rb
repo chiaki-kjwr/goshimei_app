426997e491b2758fa968270a87eb1c9ff07aa2dd
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-04-04 10:06:47 +0900

    Fix signup_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   
       def configure_permitted_parameters
         added_attrs = [:name,:email,:sex,:profile,:profile_photo,:age,:address]
-        devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+        devise_parameter_sanitizer.permit(:sign_up, keys:  %i(name))
         devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
       end
 end
