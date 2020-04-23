e70fb9d49c4aa53307d8c8eedc652864d103e3dd
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-04-23 14:41:07 +0900

    companyモデルを追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_050654) do
+ActiveRecord::Schema.define(version: 2020_04_23_054028) do
 
   create_table "chat_messages", force: :cascade do |t|
     t.string "message", null: false
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2020_04_23_050654) do
     t.index ["sender_id"], name: "index_chat_rooms_on_sender_id"
   end
 
+  create_table "companies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "company_id"
+    t.index ["company_id"], name: "index_companies_on_company_id"
+  end
+
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -48,6 +55,8 @@ ActiveRecord::Schema.define(version: 2020_04_23_050654) do
     t.datetime "updated_at", null: false
     t.text "title"
     t.integer "user_id"
+    t.integer "company_id"
+    t.index ["company_id"], name: "index_posts_on_company_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
