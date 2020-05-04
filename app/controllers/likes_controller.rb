d8304768c673897fdd091f3d725dbe182b31d84a
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-05-04 13:02:25 +0900

    お気に入り追加機能　close #16

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,19 +1,15 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
   def create
-    @like = current_user.likes.build(like_params)
-    @post = @like.post
-    if @like.save
-      respond_to :js
-    end
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.build(post_id: params[:post_id])
+    like.save
   end
 
   def destroy
-    @like = Like.find_by(id: params[:id])
-    @post = @like.post
-    if @like.destroy
-      respond_to :js
-    end
+    @post = Post.find_by(params[:post_id])
+    like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    like.destroy
   end
 
   private
