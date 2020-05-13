b344da4314e8d95f7d51904bf3da12d04b237ac7
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-05-13 19:12:19 +0900

    companyページの修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,12 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+
   def create
     @post = Post.find(params[:post_id])
     like = current_user.likes.build(post_id: params[:post_id])
     like.save
   end
+
 
   private
   def like_params
