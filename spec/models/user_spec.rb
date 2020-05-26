53a269407a5e6a4fc843ffbee127aa528388a781
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-05-26 14:55:04 +0900

    chat_room/indexの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-  describe "presence" do
-    example "ユーザーの名前が無いと、有効なデータではない" do
-      user = User.new(name: nil)
-      user.valid?
-      expect(user.errors[:name]).not_to include("can't be blank")
-    end
+
+  #describe  '正常系の機能'  do
+
+  #describe "presence" do
+  describe  '異常系の機能'  do
+    context "userが新規登録する場合" do
+      example "ユーザーの名前が無いと、有効なデータではない" do
+        user = User.new(name: nil)
+        user.valid?
+        expect(user).not_to be_valid
+        #expect(user.errors[:name]).not_to include("can't be blank")
+      end
 
     example "ユーザーのメールアドレスが無いと、有効なデータではない" do
       user = User.new(email: nil)
@@ -19,6 +25,10 @@ RSpec.describe User, type: :model do
       expect(user.errors[:email]).not_to include("can't be blank")
     end
 
+    end
+  end
+    #describe  '正常系の機能'  do
+
     describe "update_without_current_password" do
       context "user編集時にパスワードを入力しない場合" do
         example "userはプロフィール情報を編集できる" do
@@ -30,6 +40,7 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
 
   # userは複数のpostを持っている
   describe "postとのアソシエーション" do
