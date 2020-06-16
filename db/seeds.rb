9611910bb48d5d715e96f8631617d211796e0b8e
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-06-17 02:40:04 +0900

    seedのユーザーデータの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,24 +20,8 @@ name: "guest-user",
 profile_photo: open("#{Rails.root}/db/fixtures/guest.png"),
 encrypted_password:"guestuser"
 )
-end =end
-
-
-#20.times do
-  #Post.create(
-    #title: Faker::Job.field,
-    #contents: Faker::Twitter.status
-    #user_id: Faker::Number.between(1,20),
-
-#end
-
-#users = User.order(:created_at).take(3)
-#20.times do
-  #contents = Faker::Lorem.sentence(5)
-  #title= Faker::Job.field
-  #users.each { |user| user.posts.create!(contents: content) }
-#end
-
+end
+=end
 #if Rails.env == 'development'
   (1..20).each do |i|
       Post.create!(company_id:4, title: "タイトル#{i}", contents: "本文#{i}")
