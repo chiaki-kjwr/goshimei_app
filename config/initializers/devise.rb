7a2c1055bd8807bc2574b2039f25333af52509f1
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-06-19 17:31:23 +0900

    devise.rbの修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,6 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.omniauth :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET'],
-  scope: 'email',redirect_uri: "http://localhost:3000/users/auth/google_oauth2/callback"
-
+  scope: 'email',redirect_uri: 'https://goshimei-app.herokuapp.com/users/auth/google_oauth2/callback'
+  #scope: 'email',redirect_uri: "http://localhost:3000/users/auth/google_oauth2/callback"
 end
