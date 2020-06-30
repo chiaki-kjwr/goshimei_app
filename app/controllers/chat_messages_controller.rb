197080a9a14215eb6b76e6ebea340b82d4e82261
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-06-30 10:33:08 +0900

    お問い合わせページの修正

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -3,29 +3,23 @@ class ChatMessagesController < ApplicationController
 
   def create
       @chat_room = ChatRoom.find(params[:chat_room_id])
-      @chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message])
-      if user_signed_in?
-        @chat_message.is_user = true
-      elsif company_signed_in?
-        @chat_message.is_user = false
+
+      #if company_signed_in?
+        @chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user:params[:chat_message][:is_user])
+      #@chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user: params[:is_user])
+
+      #end
+
+=begin
+if company_signed_in?
+        @chat_message.is_user == false
+      #elsif company_signed_in?
+      else
+        @chat_message.is_user == true
       end
+=end
+
       redirect_to chat_room_path(@chat_room.id)
 
   end
 end
-
-
-=begin
-if @chat_message.create!
-
-      flash.now[:notice] = "チャットを送りました"
-    else
-      redirect_to chat_room_path, notice: "チャット送信に失敗しました"
-    end =end
-  endå
-
-  private
-
-  def message_params
-    params.require(:chat_message).permit(:message, :chat_room_id).merge(user_id: current_user.id)
-=end
