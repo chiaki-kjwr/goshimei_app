957c3c1828269b2dd054444269aaf2ea7c478524
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-07-01 23:46:46 +0900

    インデントの修正

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -1,12 +1,13 @@
 class ChatMessagesController < ApplicationController
   include ApplicationHelper
+  protect_from_forgery
 
   def create
       @chat_room = ChatRoom.find(params[:chat_room_id])
 
       #if company_signed_in?
-        @chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user:params[:chat_message][:is_user])
-      #@chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user: params[:is_user])
+        #@chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user:params[:chat_message][:is_user])
+        @chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user: params[:chat_message])
 
       #end
 
