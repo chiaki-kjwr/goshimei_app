b94de98ae120b01d752beb29dbec6d4fe86bb75d
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-07-02 00:41:59 +0900

    チャットメッセージコントローラの修正

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -4,23 +4,12 @@ class ChatMessagesController < ApplicationController
 
   def create
       @chat_room = ChatRoom.find(params[:chat_room_id])
-
-      #if company_signed_in?
-        #@chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user:params[:chat_message][:is_user])
-        @chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user: params[:chat_message][:is_user])
-
-      #end
-
-=begin
-if company_signed_in?
-        @chat_message.is_user == false
-      #elsif company_signed_in?
-      else
-        @chat_message.is_user == true
+      @chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user: params[:chat_message][:is_user])
+      if current_company.present?
+        @chat_message.is_user = false
+      elsif user_signed_in?
+        @chat_message.is_user = true
       end
-=end
-
       redirect_to chat_room_path(@chat_room.id)
-
   end
 end
