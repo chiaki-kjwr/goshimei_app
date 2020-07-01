20a02525d6824cb5ea8be8d763b6f74291d5e499
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-07-02 01:57:28 +0900

    userとcompanyのどちらのメッセージか識別できるよう修正

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -6,9 +6,9 @@ class ChatMessagesController < ApplicationController
       @chat_room = ChatRoom.find(params[:chat_room_id])
       @chat_message = ChatMessage.create!(chat_room_id:@chat_room.id,message: params[:chat_message][:message],is_user: params[:chat_message][:is_user])
       if current_company.present?
-        @chat_message.is_user = false
+        @chat_message.is_user = 'false'
       elsif user_signed_in?
-        @chat_message.is_user = true
+        @chat_message.is_user = 'true'
       end
       redirect_to chat_room_path(@chat_room.id)
   end
