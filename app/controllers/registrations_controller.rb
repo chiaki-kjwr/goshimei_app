ac0e89d8b427c79780940dc2b1151c1f12fe5e3a
chiaki-kjwr <58111771+chiaki-kjwr@users.noreply.github.com>
2020-07-12 08:45:28 +0900

    posts/likes.html.erbの修正

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,6 +12,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def after_update_path_for(resource)
-    user_path(current_user)
+    user_path(resource)
+    #user_path(current_user)
   end
 end
